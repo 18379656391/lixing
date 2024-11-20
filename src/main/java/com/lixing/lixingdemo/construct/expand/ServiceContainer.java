a4fe4bab55e56da627bf78b1f389d33c5c54742d
lixing41189 <lixing41189@hundsun.com>
2024-11-20 20:56:18 +0800

    [提交类型]:需求/缺陷/代码整理/代码重构/单元测试用例补充 [修改版本]: [需求单号]: [缺陷编号]: [修改内容说明]:

--- src/main/java/com/lixing/lixingdemo/construct/expand/ServiceContainer.java
+++ src/main/java/com/lixing/lixingdemo/construct/expand/ServiceContainer.java
@@ -1,11 +1,13 @@
 package com.lixing.lixingdemo.construct.expand;
 
 import com.alibaba.fastjson.JSON;
+import com.example.service.HelloService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
@@ -20,9 +22,15 @@ import java.util.Set;
 public final class ServiceContainer implements ApplicationListener<ContextRefreshedEvent> {
 
     private static final Set<String> AOP_IGNORE_SET = Collections.synchronizedSet(new HashSet<>());
+
+    @Resource
+    private HelloService helloService;
+
+
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
         log.info("aopIgnore服务:{}", JSON.toJSONString(AOP_IGNORE_SET));
+        System.out.println(helloService.sayHello("==自定义starter引入Bean成功=="));
     }
 
     public static void addAopIgnoreService(String serviceId) {
--- src/main/java/com/lixing/lixingdemo/initialDemo/ConfigLoader.java
+++ src/main/java/com/lixing/lixingdemo/initialDemo/ConfigLoader.java
@@ -1,12 +1,15 @@
 package com.lixing.lixingdemo.initialDemo;
 
+import com.example.service.HelloService;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
 
 /**
  * @version: 1.0
